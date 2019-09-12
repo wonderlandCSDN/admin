92a55b85ccac63f458ce709ef46cf4f39ac66c6b
luoyanfei <luoyanfei@capol.cn>
2019-09-12 14:11:26 +0800

    执行脚本配置

--- config/index.js
+++ config/index.js
@@ -6,16 +6,15 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
 
-    // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    // 各种设备设置信息
+    host: '0.0.0.0', // 主机号
+    port: 8080, // 端口号（默认8080）
+    autoOpenBrowser: true, // 是否自动打开浏览器
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
